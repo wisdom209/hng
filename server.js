14a62e9c0d69e48ec9fdb3a368a1a24b8239e548
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-09-07 11:36:53 +0100

    update

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+app.get('/api', (req, res) => {
+	const { slack_name, track } = req.query;
+	const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	
+	const responseObject = {
+		slack_name,
+		current_day: days[new Date(Date.now()).getDay()],
+		utc_time: new Date(Date.now()).toISOString().slice(0, -5) + 'Z',
+		track,
+		github_file_url: 'https://github.com/wisdom209/hng/blob/main/server.js',
+		github_repo_url: 'https://github.com/wisdom209/hng',
+		status_code: 200
+	};
+
+	res.status(200).json(responseObject);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
